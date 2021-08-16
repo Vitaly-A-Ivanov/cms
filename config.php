cdd9c7528af4fffb58f6cf32e7e35667ee1e9381
Vitaly Ivanov <viv55941@icloud.com>
2021-08-16 15:07:35 +0100

    database update

--- config.php
+++ config.php
@@ -2,8 +2,8 @@
 
 // database settings
 try {
-    $conn = new PDO('mysql:host=c8u4r7fp8i8qaniw.chr7pe7iynqr.eu-west-1.rds.amazonaws.com:3306;dbname=r9kd9m8pvsmvrk4j',
-        'eg09y9eqjac9hpy9', 'hnch1nfcsgl2zm5r', [
+    $conn = new PDO('mysql:host=pei17y9c5bpuh987.chr7pe7iynqr.eu-west-1.rds.amazonaws.com:3306;dbname=x0g4hw2ntc0cma7w',
+        'ko8gvu5iqafgcwlr', 'ctsjrf37oqrknwvo', [
             PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
             PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
         ]);
